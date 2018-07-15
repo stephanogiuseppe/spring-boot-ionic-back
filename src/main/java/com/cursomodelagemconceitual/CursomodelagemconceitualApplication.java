72582d211c9e14247d72012442ef6a857b94cbb9
Stéphano Giuseppe <stephano.grp@gmail.com>
2018-07-15 19:32:08 -0300

    Criação do projeto

---
+++ src/main/java/com/cursomodelagemconceitual/CursomodelagemconceitualApplication.java
@@ -0,0 +1,12 @@
+package com.cursomodelagemconceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomodelagemconceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomodelagemconceitualApplication.class, args);
+	}
+}
