7b6b9c525c1dfeb708fc4f574e94e461d8ff7008
Stéphano Giuseppe <stephano.grp@gmail.com>
2018-07-15 20:16:01 -0300

    Testes de classe de dominio Categoria

--- src/main/java/com/cursomodelagemconceitual/resources/CategoriaResource.java
+++ src/main/java/com/cursomodelagemconceitual/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.cursomodelagemconceitual.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cursomodelagemconceitual.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST works!";
+	public List<Categoria> listar() {
+		
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(c1);
+		categorias.add(c2);
+		
+		return categorias;
 	}
 }
