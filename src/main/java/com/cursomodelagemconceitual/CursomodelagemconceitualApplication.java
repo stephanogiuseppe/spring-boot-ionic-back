2273eb446f87ca3a5fb415d3232446c56c99b217
Stéphano Giuseppe <stephano.grp@gmail.com>
2018-07-16 14:04:39 -0300

    Criação de classe Produto e associações de produtos com categorias

--- src/main/java/com/cursomodelagemconceitual/CursomodelagemconceitualApplication.java
+++ src/main/java/com/cursomodelagemconceitual/CursomodelagemconceitualApplication.java
@@ -9,13 +9,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cursomodelagemconceitual.domain.Categoria;
+import com.cursomodelagemconceitual.domain.Produto;
 import com.cursomodelagemconceitual.repositories.CategoriaRepository;
+import com.cursomodelagemconceitual.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomodelagemconceitualApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomodelagemconceitualApplication.class, args);
@@ -27,7 +31,19 @@ public class CursomodelagemconceitualApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 	
 	
--- src/main/java/com/cursomodelagemconceitual/repositories/ProdutoRepository.java
+++ src/main/java/com/cursomodelagemconceitual/repositories/ProdutoRepository.java
@@ -3,9 +3,9 @@ package com.cursomodelagemconceitual.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.cursomodelagemconceitual.domain.Categoria;
+import com.cursomodelagemconceitual.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
 }
