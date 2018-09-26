f63bde53158ca215815298922850eabfeb713a98
stephanogiuseppe <stephano.grp@gmail.com>
2018-09-25 23:23:54 -0300

    Foram adicionados os verbos GET, PUT e DELETE em Clientes

--- src/main/java/com/cursomodelagemconceitual/resources/ClienteResource.java
+++ src/main/java/com/cursomodelagemconceitual/resources/ClienteResource.java
@@ -1,15 +1,24 @@
 package com.cursomodelagemconceitual.resources;
 
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cursomodelagemconceitual.domain.Cliente;
+import com.cursomodelagemconceitual.dto.ClienteDTO;
 import com.cursomodelagemconceitual.services.ClienteService;
 
 @RestController
@@ -26,12 +35,50 @@ public class ClienteResource {
 		
 		return ResponseEntity.ok().body(cliente);
 	}
-	
-	@RequestMapping
-	public ResponseEntity<List<Cliente>> findAll() {
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO clienteDTO) {
+		Cliente cliente = clienteService.fromDTO(clienteDTO);
+		cliente = clienteService.insert(cliente);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO clienteDTO, @PathVariable Integer id) {
+		Cliente cliente = clienteService.fromDTO(clienteDTO); 
+		cliente.setId(id);
+		clienteService.update(cliente);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		clienteService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
 		
 		List<Cliente> clientes = clienteService.findAll();
 		
-		return ResponseEntity.ok().body(clientes);
+		List<ClienteDTO> clientesDto = clientes.stream().map(cliente -> new ClienteDTO(cliente)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(clientesDto);
+	}
+
+	@RequestMapping(value="/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "size", defaultValue = "24") Integer size,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		
+		Page<Cliente> clientes = clienteService.findPage(page, size, orderBy, direction);
+
+		Page<ClienteDTO> clientesDto = clientes.map(cliente -> new ClienteDTO(cliente));
+		
+		return ResponseEntity.ok().body(clientesDto);
 	}
 }
--- src/main/java/com/cursomodelagemconceitual/services/ClienteService.java
+++ src/main/java/com/cursomodelagemconceitual/services/ClienteService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cursomodelagemconceitual.domain.Cliente;
+import com.cursomodelagemconceitual.dto.ClienteDTO;
 import com.cursomodelagemconceitual.repositories.ClienteRepository;
+import com.cursomodelagemconceitual.services.exceptions.DataIntegrityException;
 import com.cursomodelagemconceitual.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,4 +37,41 @@ public class ClienteService {
 			new ObjectNotFoundException("Objeto não encontrado. Id: " + id + ", tipo: " + Cliente.class.getName())
 		);
 	}
+
+	public Cliente insert(Cliente cliente) {
+		cliente.setId(null);
+		return clienteRepository.save(cliente);
+	}
+
+	public Cliente update(Cliente cliente) {
+		Cliente newCliente = find(cliente.getId());
+		updateData(newCliente, cliente);
+		return clienteRepository.save(newCliente);	
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			clienteRepository.deleteById(id);			
+		} catch (DataIntegrityViolationException exception) {
+			throw new DataIntegrityException("Não é possível excluir o cliente, pois há entidades relacionadas");
+		}
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer size, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, size, Direction.valueOf(direction), orderBy);
+		
+		return clienteRepository.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
+
+	private void updateData(Cliente newCliente, Cliente cliente) {
+		newCliente.setNome(cliente.getNome());
+		newCliente.setEmail(cliente.getEmail());
+	}
+
 }
